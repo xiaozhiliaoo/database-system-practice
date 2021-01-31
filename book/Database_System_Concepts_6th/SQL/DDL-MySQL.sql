2be75f468b20cb514b9420226bd9963c6f66fa98
lili <xiaozhiliaoo@gmail.com>
2021-01-31 17:23:12 +0800

    up

--- book/Database_System_Concepts_6th/SQL/DDL-MySQL.sql
+++ book/Database_System_Concepts_6th/SQL/DDL-MySQL.sql
@@ -1,105 +1,127 @@
-create table classroom
-	(building		varchar(15),
-	 room_number		varchar(7),
-	 capacity		numeric(4,0),
-	 primary key (building, room_number)
-	);
-create table department
-	(dept_name		varchar(20), 
-	 building		varchar(15), 
-	 budget		        numeric(12,2) check (budget > 0),
-	 primary key (dept_name)
-	);
-create table course
-	(course_id		varchar(8), 
-	 title			varchar(50), 
-	 dept_name		varchar(20),
-	 credits		numeric(2,0) check (credits > 0),
-	 primary key (course_id),
-	 foreign key (dept_name) references department(dept_name)
-		on delete set null
-	);
-create table instructor
-	(ID			varchar(5), 
-	 name			varchar(20) not null, 
-	 dept_name		varchar(20), 
-	 salary			numeric(8,2) check (salary > 29000),
-	 primary key (ID),
-	 foreign key (dept_name) references department(dept_name)
-		on delete set null
-	);
-create table section
-	(course_id		varchar(8), 
-         sec_id			varchar(8),
-	 semester		varchar(6)
-		check (semester in ('Fall', 'Winter', 'Spring', 'Summer')), 
-	 year			numeric(4,0) check (year > 1701 and year < 2100), 
-	 building		varchar(15),
-	 room_number		varchar(7),
-	 time_slot_id		varchar(4),
-	 primary key (course_id, sec_id, semester, year),
-	 foreign key (course_id) references course(course_id)
-		on delete cascade,
-	 foreign key (building, room_number) references classroom(building, room_number)
-		on delete set null
-	);
-create table teaches
-	(ID			varchar(5), 
-	 course_id		varchar(8),
-	 sec_id			varchar(8), 
-	 semester		varchar(6),
-	 year			numeric(4,0),
-	 primary key (ID, course_id, sec_id, semester, year),
-	 foreign key (course_id,sec_id, semester, year) references section(course_id,sec_id, semester, year)
-		on delete cascade,
-	 foreign key (ID) references instructor(ID)
-		on delete cascade
-	);
-create table student
-	(ID			varchar(5), 
-	 name			varchar(20) not null, 
-	 dept_name		varchar(20), 
-	 tot_cred		numeric(3,0) check (tot_cred >= 0),
-	 primary key (ID),
-	 foreign key (dept_name) references department(dept_name)
-		on delete set null
-	);
-create table takes
-	(ID			varchar(5), 
-	 course_id		varchar(8),
-	 sec_id			varchar(8), 
-	 semester		varchar(6),
-	 year			numeric(4,0),
-	 grade		        varchar(2),
-	 primary key (ID, course_id, sec_id, semester, year),
-	 foreign key (course_id,sec_id, semester, year) references section(course_id,sec_id, semester, year)
-		on delete cascade,
-	 foreign key (ID) references student(ID)
-		on delete cascade
-	);
-create table advisor
-	(s_ID			varchar(5),
-	 i_ID			varchar(5),
-	 primary key (s_ID),
-	 foreign key (i_ID) references instructor (ID)
-		on delete set null,
-	 foreign key (s_ID) references student (ID)
-		on delete cascade
-	);
-create table time_slot
-	(time_slot_id		varchar(4),
-	 day			varchar(1),
-	 start_hr		numeric(2) check (start_hr >= 0 and start_hr < 24),
-	 start_min		numeric(2) check (start_min >= 0 and start_min < 60),
-	 end_hr			numeric(2) check (end_hr >= 0 and end_hr < 24),
-	 end_min		numeric(2) check (end_min >= 0 and end_min < 60),
-	 primary key (time_slot_id, day, start_hr, start_min)
-	);
-create table prereq
-	(course_id		varchar(8), 
-	 prereq_id		varchar(8),
-	 primary key (course_id, prereq_id),
-	 foreign key (course_id) references course(course_id)
-		on delete cascade,
-	 foreign key (prereq_id) references course(course_id)
-	)
+CREATE TABLE classroom (
+    building VARCHAR(15),
+    room_number VARCHAR(7),
+    capacity NUMERIC(4 , 0 ),
+    PRIMARY KEY (building , room_number)
+);
+
+CREATE TABLE department (
+    dept_name VARCHAR(20),
+    building VARCHAR(15),
+    budget NUMERIC(12 , 2 ) CHECK (budget > 0),
+    PRIMARY KEY (dept_name)
+);
+
+CREATE TABLE course (
+    course_id VARCHAR(8),
+    title VARCHAR(50),
+    dept_name VARCHAR(20),
+    credits NUMERIC(2 , 0 ) CHECK (credits > 0),
+    PRIMARY KEY (course_id),
+    FOREIGN KEY (dept_name)
+        REFERENCES department (dept_name)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE instructor (
+    ID VARCHAR(5),
+    name VARCHAR(20) NOT NULL,
+    dept_name VARCHAR(20),
+    salary NUMERIC(8 , 2 ) CHECK (salary > 29000),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (dept_name)
+        REFERENCES department (dept_name)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE section (
+    course_id VARCHAR(8),
+    sec_id VARCHAR(8),
+    semester VARCHAR(6) CHECK (semester IN ('Fall' , 'Winter', 'Spring', 'Summer')),
+    year NUMERIC(4 , 0 ) CHECK (year > 1701 AND year < 2100),
+    building VARCHAR(15),
+    room_number VARCHAR(7),
+    time_slot_id VARCHAR(4),
+    PRIMARY KEY (course_id , sec_id , semester , year),
+    FOREIGN KEY (course_id)
+        REFERENCES course (course_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (building , room_number)
+        REFERENCES classroom (building , room_number)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE teaches (
+    ID VARCHAR(5),
+    course_id VARCHAR(8),
+    sec_id VARCHAR(8),
+    semester VARCHAR(6),
+    year NUMERIC(4 , 0 ),
+    PRIMARY KEY (ID , course_id , sec_id , semester , year),
+    FOREIGN KEY (course_id , sec_id , semester , year)
+        REFERENCES section (course_id , sec_id , semester , year)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ID)
+        REFERENCES instructor (ID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE student (
+    ID VARCHAR(5),
+    name VARCHAR(20) NOT NULL,
+    dept_name VARCHAR(20),
+    tot_cred NUMERIC(3 , 0 ) CHECK (tot_cred >= 0),
+    PRIMARY KEY (ID),
+    FOREIGN KEY (dept_name)
+        REFERENCES department (dept_name)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE takes (
+    ID VARCHAR(5),
+    course_id VARCHAR(8),
+    sec_id VARCHAR(8),
+    semester VARCHAR(6),
+    year NUMERIC(4 , 0 ),
+    grade VARCHAR(2),
+    PRIMARY KEY (ID , course_id , sec_id , semester , year),
+    FOREIGN KEY (course_id , sec_id , semester , year)
+        REFERENCES section (course_id , sec_id , semester , year)
+        ON DELETE CASCADE,
+    FOREIGN KEY (ID)
+        REFERENCES student (ID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE advisor (
+    s_ID VARCHAR(5),
+    i_ID VARCHAR(5),
+    PRIMARY KEY (s_ID),
+    FOREIGN KEY (i_ID)
+        REFERENCES instructor (ID)
+        ON DELETE SET NULL,
+    FOREIGN KEY (s_ID)
+        REFERENCES student (ID)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE time_slot (
+    time_slot_id VARCHAR(4),
+    day VARCHAR(1),
+    start_hr NUMERIC(2) CHECK (start_hr >= 0 AND start_hr < 24),
+    start_min NUMERIC(2) CHECK (start_min >= 0 AND start_min < 60),
+    end_hr NUMERIC(2) CHECK (end_hr >= 0 AND end_hr < 24),
+    end_min NUMERIC(2) CHECK (end_min >= 0 AND end_min < 60),
+    PRIMARY KEY (time_slot_id , day , start_hr , start_min)
+);
+
+CREATE TABLE prereq (
+    course_id VARCHAR(8),
+    prereq_id VARCHAR(8),
+    PRIMARY KEY (course_id , prereq_id),
+    FOREIGN KEY (course_id)
+        REFERENCES course (course_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (prereq_id)
+        REFERENCES course (course_id)
+)
---
+++ book/Database_System_Concepts_6th/SQL/chapter.sql
@@ -0,0 +1,35 @@
+select name,instructor.dept_name,building from instructor,department where instructor.dept_name=department.dept_name;
+select * from instructor,teaches ;
+select name,course_id from instructor,teaches where instructor.ID = teaches.ID;
+select name,course_id from instructor natural join teaches;
+select name,title from instructor natural join teaches,course where teaches.course_id=course.course_id;
+select name,title from (instructor natural join teaches) join course using(course_id);
+
+select name,title from instructor natural join teaches natural join course;
+select distinct T.name from instructor as T, instructor as S where T.salary > S.salary and S.dept_name='Biology';
+(select course_id from section where semester='Fall' and year=2009) union (select course_id from section where semester='Spring' and year=2010) ;
+-- (select course_id from section where semester='Fall' and year=2009) union all (select course_id from section where semester='Spring' and year=2010) ;
+-- (select course_id from section where semester='Fall' and year=2009) except (select course_id from section where semester='Spring' and year=2010) ;
+select count(distinct ID) from teaches where semester='Spring' and year=2010;
+select dept_name,avg(salary) from instructor group by dept_name;
+select avg(salary) from instructor;
+select dept_name,count(distinct ID) from instructor natural join teaches where semester='Spring' and year=2010 group by dept_name;
+select dept_name,avg(salary) from instructor group by dept_name having avg(salary) > 42000;
+select course_id,semester,year,sec_id,avg(tot_cred) from takes natural join student where year=2009 group by course_id,semester,year,sec_id having count(ID) >=2;
+select distinct course_id from section where semester='Spring' and year=2009 and course_id in(select course_id from section where semester='Spring' and year=2010);
+select distinct course_id from section where semester='Spring' and year=2009 and course_id not in(select course_id from section where semester='Spring' and year=2010);
+select count(distinct ID) from takes where (course_id,sec_id,year) in (select course_id,sec_id,year from teaches where teaches.ID=10101);
+select name from instructor where salary > some(select salary from instructor where dept_name='Biology');
+select name from instructor where salary > all(select salary from instructor where dept_name='Biology');
+select course_id from section as S where semester='Fall' and year=2009 and exists(select * from section as T where  semester='Spring' and year=2010 and S.course_id=T.course_id);
+select dept_name, avg_salary from (select dept_name, avg(salary)as avg_salary from instructor  group by dept_name) as A where avg_salary>42000;
+update instructor set salary = (case when salary <=10000 then salary * 1.05 else salary * 1.03 end);
+select * from student left join takes on student.ID=takes.ID;
+select * from student left join takes on true where student.ID=takes.ID;
+
+
+
+
+
+
+
